9427578c5cb10aa8b30e07f66d5bcbe3c98487de
1996LM <lmiguel.hoyos@gmail.com>
2022-08-31 18:18:14 -0500

    Âcommit inicial

---
+++ main.py
@@ -0,0 +1,8 @@
+#primero se debe creqr un entorno virtual 
+#usamos virtualenviroment(venv)
+#venv es el nombre por defecto que trae la libreria virtual enviroment
+#venv  venv es el nombre mas comun pero se le puede colocar cualquiera
+#un entorno virtual es lugar donde coloco las librerias especificas con sus versiones especificas que debo utilizar en el proyecto
+#porque en un futuro para poder correr son necesarias las librerias
+#.tambien para aislar la libreria
+#
